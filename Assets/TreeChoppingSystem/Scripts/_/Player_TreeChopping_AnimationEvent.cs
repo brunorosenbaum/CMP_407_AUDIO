798cd9d2862dd65db63067b1d7ca7be0e712d465
brunorosenbaum11 <2000512@uad.ac.uk>
2023-11-27 21:47:50 +0000

    First commit scene setup

---
+++ Assets/TreeChoppingSystem/Scripts/_/Player_TreeChopping_AnimationEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_TreeChopping_AnimationEvent : MonoBehaviour {
+
+    public event EventHandler OnHit;
+
+
+    public void Hit() {
+        OnHit?.Invoke(this, EventArgs.Empty);
+    }
+
+}
